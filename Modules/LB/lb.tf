d04ed644ea8e34543b634d3c685e6c534cb68e1f
anantadurgaprasadar <anantadurgaprasadar@gmail.com>
2022-09-12 16:38:29 +0530

    added alb

---
+++ Modules/LB/lb.tf
@@ -0,0 +1,37 @@
+resource "aws_lb" "load_balancer" {
+  name               = "${var.lb-name}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = "${var.lb-sg}"
+  subnets            = "${var.lb-subnets}"
+
+  enable_deletion_protection = true
+
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "test" {
+  name     = "${var.tg-name}"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc-id}"
+}
+
+resource "aws_lb_listener" "test" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
+}
---
+++ Modules/LB/output.tf
@@ -0,0 +1,8 @@
+output "lb-arn" {
+    value = aws_lb.load_balancer.arn
+  
+}
+output "tg-arn" {
+    value = aws_lb_target_group.test.arn
+  
+}
